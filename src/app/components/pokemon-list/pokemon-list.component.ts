f1cef8832c1f06af09181ad1b7a48d061156844f
blurry <tamsilansari29@gmail.com>
2025-06-07 19:50:49 +0530

    Optimized api calls

--- src/app/components/pokemon-list/pokemon-list.component.ts
+++ src/app/components/pokemon-list/pokemon-list.component.ts
@@ -1,53 +1,63 @@
 import { ActivatedRoute, Router } from '@angular/router';
 import { PokemonListService } from './../../services/pokemon-list.service';
-import { Component, OnInit } from '@angular/core';
-import { map} from 'rxjs/operators';
-
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
-  styleUrls: ['./pokemon-list.component.css']
+  styleUrls: ['./pokemon-list.component.css'],
 })
-export class PokemonListComponent implements OnInit {
+export class PokemonListComponent implements OnInit, OnDestroy {
   config: any;
   pokemons: any[] = [];
-  filteredPokemons: any[] = []
-  page = 1;
-  pageSize= 21;
-  totalPokemons!: number;
+  filteredPokemons: any[] = [];
+  pageSize = 20;
   filterTerm!: string;
+  sub: Subscription;
 
-  constructor(private service: PokemonListService, private route: ActivatedRoute, private router: Router) { 
-    this.config ={
+  constructor(
+    private service: PokemonListService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.config = {
       currentPage: 1,
       itemsPerPage: this.pageSize,
-      totalItems: this.totalPokemons
-    }
+      totalItems: this.pageSize,
+    };
 
-    this.route.queryParamMap
-      .pipe(map(params => params.get('page')))
-      .subscribe(page => this.config.currentPage = page);
+    this.sub = this.route.queryParamMap
+      .pipe(map((params) => params.get('page')))
+      .subscribe(async (page) => {
+        console.log({ page });
+        this.config.currentPage = page;
+        await this.getPokemons();
+      });
   }
 
-  ngOnInit(): void {
-    this.getPokemons()
+  async ngOnInit(): Promise<void> {
+    await this.getPokemons();
   }
 
-  getPokemons(){
-    this.service.getPokemons(1126, this.page + 0).subscribe((response: any) => {
-      this.totalPokemons = response.count;
-      this.filteredPokemons = this.pokemons
-      console.log(this.totalPokemons)
+  ngOnDestroy(): void {
+    if (this.sub) this.sub.unsubscribe();
+  }
 
-      response.results.forEach((pokemon: any) => {
-        this.service.getUniquePokemon(pokemon.name)
-        .subscribe((uniquePokemon: any) => {
-          this.pokemons.push(uniquePokemon);
-          // console.log(this.pokemons)
-        })
-      })
-    })
+  async getPokemons() {
+    const res = (await this.service
+      .getPokemons(this.pageSize, (this.config.currentPage - 1) * this.pageSize)
+      ?.toPromise()) as any;
+    this.config.totalItems = res.count;
+    const pokemons: any[] = [];
+    res?.results?.forEach((pok: any) => {
+      this.service
+        .getUniquePokemon(pok.name)
+        ?.toPromise()
+        ?.then((upok) => pokemons.push(upok));
+    });
+    this.filteredPokemons = pokemons;
   }
 
   getType(pokemon: any): string {
@@ -55,25 +65,34 @@ export class PokemonListComponent implements OnInit {
   }
 
   // filter(filterTerm: string){
-  //   this.filteredPokemons = (filterTerm) ? 
+  //   this.filteredPokemons = (filterTerm) ?
   //   this.pokemons.filter(pokemon => pokemon.name.includes(this.filterTerm)) : this.pokemons
   // }
 
-  filter(filterTerm: string){
-    if (filterTerm){
-      this.config.currentPage = 1
-      this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name.includes(this.filterTerm.toLocaleLowerCase()))
-    }
-    else{
-      this.route.queryParamMap.pipe(map(params=> params.get('page')))
-      .subscribe(page => this.config.currentPage = page);
-      this.filteredPokemons = this.pokemons
+  async filter(filterTerm: string) {
+    if (filterTerm) {
+      this.config.currentPage = 1;
+      this.filteredPokemons = [];
+      const res = (await this.service.getPokemons(1302, 0)?.toPromise()) as any;
+      const matchedPokii = res?.results?.filter((pokemon: any) =>
+        pokemon.name.includes(this.filterTerm.toLocaleLowerCase())
+      );
+
+      this.config.itemsPerPage = matchedPokii.length;
+      this.config.totalItems = matchedPokii.length;
+      matchedPokii?.forEach(async (pok: any) => {
+        this.service
+          .getUniquePokemon(pok.name)
+          ?.toPromise()
+          ?.then((upok) => this.filteredPokemons.push(upok));
+      });
+    } else {
+      this.config.currentPage = this.route.snapshot.queryParamMap.get('page');
+      await this.getPokemons();
     }
   }
 
   pageChange(newPage: number) {
-		this.router.navigate(['/pokemon-list'], { queryParams: { page: newPage } });
-	}
-
+    this.router.navigate(['/pokemon-list'], { queryParams: { page: newPage } });
+  }
 }
-
